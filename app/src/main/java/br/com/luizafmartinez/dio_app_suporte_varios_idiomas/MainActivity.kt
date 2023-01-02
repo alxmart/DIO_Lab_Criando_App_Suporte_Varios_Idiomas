4c498f910efa201219ff1afe5bda418f256cce4b
alxmart <alxmartinez@gmail.com>
2023-01-02 16:14:05 -0300

    First commit. Project created.

---
+++ app/src/main/java/br/com/luizafmartinez/dio_app_suporte_varios_idiomas/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.luizafmartinez.dio_app_suporte_varios_idiomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
